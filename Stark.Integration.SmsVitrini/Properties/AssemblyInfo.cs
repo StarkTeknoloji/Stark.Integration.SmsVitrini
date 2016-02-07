40396f704d829566cee7137a90ba91d136cfb268
Ömer Cinbat <ocinbat@outlook.com>
2016-02-07 12:05:06 +0200

    Assembly information updated.

--- Stark.Integration.SmsVitrini/Properties/AssemblyInfo.cs
+++ Stark.Integration.SmsVitrini/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Stark.Integration.SmsVitrini")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .Net client for SmsVitrini api.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Stark Teknoloji Sanayi ve Ticaret Limited Şirketi")]
 [assembly: AssemblyProduct("Stark.Integration.SmsVitrini")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
